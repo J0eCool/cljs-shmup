7479981fd28d5efb7d11d7255272712ee01ed964
J0eCool <count.j0ecool@gmail.com>
2014-02-25 00:15:34 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject cljs-shmup "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2138"]]
+
+  :plugins [[lein-cljsbuild "1.0.2"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild { 
+    :builds [{:id "cljs-shmup"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "cljs_shmup.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}]})
---
+++ src/cljs_shmup/core.cljs
@@ -0,0 +1,58 @@
+(ns cljs-shmup.core)
+
+(defn time-since [then]
+  (/ (-
+      (js/Date.now)
+      then)
+     1000))
+
+(defn set-text [id text]
+  (set!
+   (.-innerText (.getElementById js/document id))
+   text))
+
+(defn set-html [id html]
+  (set!
+   (.-innerHTML (.getElementById js/document id))
+   html))
+
+(defn init []
+  (set-html
+   "game-body"
+   "<div class=\"box\" id=\"player\"></div>"))
+
+(defn update-text [dT]
+  ;(set-text "text" (str dT))
+  )
+
+(let [t (atom 0)]
+  (defn update-box [dT]
+    (swap! t (partial + dT))
+    (let [box (.getElementById js/document "player")
+          style (.-style box)]
+      (set! (.-left style)
+            (str
+             (+ 200 (* 100 (Math/cos @t)))
+             "px"))
+      (set! (.-top style) "500px"))))
+
+(.getElementById js/document "box")
+
+(let [lastFrame (atom (js/Date.now))]
+  (defn update []
+    (let [dT (time-since @lastFrame)]
+      (update-text dT)
+      (update-box dT))
+    (swap! lastFrame js/Date.now)))
+
+(init)
+(.setInterval js/window update 33)
+(set!
+ (.-onmousemove js/window)
+ (fn [e]
+   (set-html
+    "text"
+    (str
+     (.-clientX e)
+     ", "
+     (.-clientY e)))))
